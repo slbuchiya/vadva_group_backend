0ad6ddda986497e57ecac9952dc58c69f002cc8b
BUCHIYA SACHIN <buchiyasachin8@gmail.com>
2026-02-09 22:12:40 +0530

    changes

---
+++ backend/test-mongo.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+// URI provided by user
+const uri = "mongodb+srv://slbuchiya:slbuchiya@govindsuragroup.ghhvytp.mongodb.net/tshirt_store?retryWrites=true&w=majority&appName=GovindSuraGroup";
+
+console.log("Attempting to connect...");
+
+mongoose.connect(uri)
+    .then(() => {
+        console.log("Successfully connected!");
+        process.exit(0);
+    })
+    .catch(err => {
+        console.error("Connection failed:", err.message);
+        console.error("Full error:", err);
+        process.exit(1);
+    });
