424261a120d621f576b35f8b93cbc943adea4b43
BUCHIYA SACHIN <buchiyasachin8@gmail.com>
2026-02-10 09:51:28 +0530

    mongo update

--- backend/models/Settings.js
+++ backend/models/Settings.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const settingsSchema = new mongoose.Schema({
     key: { type: String, required: true, unique: true }, // e.g., 'upi_id'
-    value: { type: String, required: true }
+    value: { type: String, default: '' }
 });
 
 module.exports = mongoose.model('Settings', settingsSchema);
